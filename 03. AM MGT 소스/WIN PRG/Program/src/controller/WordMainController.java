dfc19f95df665a064fa14e3fd3df9c1fa3360b3a
USIS-Dabin <39392247+yoondabin@users.noreply.github.com>
2018-06-18 10:37:13 +0900

    update4

--- 03. AM MGT 소스/WIN PRG/Program/src/controller/WordMainController.java
+++ 03. AM MGT 소스/WIN PRG/Program/src/controller/WordMainController.java
@@ -7,16 +7,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
-public class WordMainController {
+
+	public class WordMainController {
 	@FXML
 	private TableView<Word> wordTable;
 	@FXML
@@ -35,11 +36,9 @@ public class WordMainController {
 	private TableColumn<Load, String> time;
 	private Main main;
 	private ObservableList<Load> loadList = FXCollections.observableArrayList();
-	public static String textData = "C:\\AMlog\\";
-
-	
-	
+	private int checkNumber;
 	private List<WatchServiceThread> threadList;
+	public static String textData = "C:\\AMlog\\";
 	@FXML
 	private void initialize() {
 		now.setCellValueFactory(cellData -> cellData.getValue().getNowProperty());
@@ -53,8 +52,8 @@ public class WordMainController {
 		this.main = main;
 		wordTable.setItems(main.getWordList());
 		loadTable.setItems(loadList);
-		
 	}
+	
 	public WordMainController() {     }	
 	@FXML
 	private void addAction() {  // 추가 버튼 활성화
@@ -62,6 +61,7 @@ public class WordMainController {
 		int returnValue = main.setWordDataView(word);
 		if(returnValue ==1) {
 			main.getWordList().add(word);
+			
 			File filter = new File(textData); 
 			if (!filter.exists()) {
 				filter.mkdir();
@@ -71,14 +71,10 @@ public class WordMainController {
 					fw = new BufferedWriter(new FileWriter(textData+"파일경로.txt",true));
 					fw.append("현재경로 : "+word.getNow()+",  이동경로 : "+word.getNext()+"\r\n");
 					fw.flush();
-					fw.close();
-				} catch (IOException e) {
-					
+					fw.close();										
+				} catch (IOException e) {	
 					e.printStackTrace();
 				}
-				
-				
-				
 			}	
 		}
 	}
@@ -86,20 +82,43 @@ public class WordMainController {
 	private void editAction() { // 수정 버튼 활성화
 		Word word = wordTable.getSelectionModel().getSelectedItem();
 		if (word != null) {
-			main.setWordDataView(word);
+			main.setWordDataView(word);		
 		}
 	}
 	@FXML
 	private void deleteAction() { //삭제 버튼 활성화
 		int selectedIndex = wordTable.getSelectionModel().getSelectedIndex();
 		if (selectedIndex >=0) {
+			
+			// 1. value_Path_all문자에서 selectedIndex에 해당하는 문자를 찾는다. 
+			// 2. this.main.value_Path_all.indexOf("")로 찾는다.
+			// 3. this.main.value_Path_all.replace("C:\\test|C:\\test2", "");
+			String path1 = now.getCellData(selectedIndex)+"|"+next.getCellData(selectedIndex);
+			
+			if(this.main.value_Path_all.indexOf(selectedIndex) == 0)
+			{
+				this.main.value_Path_all.replace(now.getCellData(selectedIndex)+"[|]"+next.getCellData(selectedIndex), "");
+				
+				if(this.main.value_Path_all.length() > 0) {
+					// 맨앞에 있는 "|"빼고 다음부터 저장.
+					this.main.value_Path_all = this.main.value_Path_all.substring(1);
+				}
+			} else {
+				// 중간에 포함된 경로이기 때문에 앞에 "|"를 붙이고 같이 삭제.
+				this.main.value_Path_all.replace(now.getCellData(selectedIndex)+"[|]"+next.getCellData(selectedIndex)," ");
+			}
+//			this.main.settings.put(this.main.value_Path_all,"");
+			System.out.println(path1);
+			System.out.println("메인 :"+main.value_Path_all);
+			System.out.println("셀렉"+selectedIndex);
+
 			wordTable.getItems().remove(selectedIndex);
 		}else {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.initOwner(main.getPrimaryStage());
 			alert.setTitle("오류 메시지");
 			alert.setHeaderText("선택 오류가 발생했습니다.");
-			alert.setContentText("삭제 할 메뉴를 선태해주세요");
+			alert.setContentText("삭제 할 메뉴를 선택해주세요");
 			alert.showAndWait();
 		}
 	}
@@ -108,9 +127,24 @@ public class WordMainController {
 	}
 	
 	@FXML
-	private void startAction(ActionEvent a) { // 액션 시작 버튼
+	private void startAction() { // 액션 시작 버튼
+		if(main.wordList().size()==0) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(main.getPrimaryStage());
+			alert.setTitle("오류 메시지");
+			alert.setHeaderText("지정된 경로가 없습니다.");
+			alert.setContentText("경로를 추가해주세요");
+			alert.showAndWait();
+		}else if( checkNumber>1 ) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(main.getPrimaryStage());
+			alert.setTitle("오류 메시지");
+			alert.setHeaderText("오류가 발생 했습니다");
+			alert.setContentText("중지 버튼을 누른 후 실행해 주세요");
+			alert.showAndWait();
+		}else {
+			checkNumber=2;
 		WatchServiceThread wst;
-		
 		// thread 저장 ArrayList
 		threadList = new ArrayList<WatchServiceThread>();
 		
@@ -123,12 +157,13 @@ public class WordMainController {
 			// Thread 실행 후 ArrayList에 저장.
 			threadList.add(wst);
 		}
+		}	
 	}
 	
 	@FXML
 	private void stopAction() { // 액션 정지버튼
 		WatchServiceThread wst;
-		
+		checkNumber=0;
 		// Stop 명령을 실행할 때 실행중인 Thread 모두 종료.
 		for (int iCount = 0; iCount < threadList.size(); iCount++) {
 			// Thread 생성
@@ -144,17 +179,12 @@ public class WordMainController {
 	// WatchServerThread 클래스에서 참조해서 사용
 	public void addRowInloadList(String fileName, String dirPath, String action, String date,String logtime){	
 		loadList.add(new Load(fileName, dirPath, action, date));
-		
-		
-		try { // 로그 저장
-			
 				
+		try { // 로그 저장
 				BufferedWriter fw = new BufferedWriter(new FileWriter(textData+logtime+".txt",true));
 				fw.append(fileName+", "+dirPath+", "+action+", "+date+"\r\n");
 				fw.flush();
 				fw.close();
-			
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
