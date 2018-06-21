13220b364b9e3e1ce3a77bded3c256e47303891f
USIS-WOOJEONG <39390819+USIS-WOOJEONG@users.noreply.github.com>
2018-06-21 17:33:02 +0900

    180621 코드 수정
    180621 코드 수정

--- 03. AM MGT 소스/LINUX PRG/fxMongoDB/src/model/Connection.java
+++ 03. AM MGT 소스/LINUX PRG/fxMongoDB/src/model/Connection.java
@@ -1,13 +1,9 @@
 package model;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
@@ -15,6 +11,8 @@ public class Connection {
 	public String MongoDB_IP = "127.0.0.1";
 	public int MongoDB_PORT = 27017;
 	public DB MongoDB_DB = null;
+	public String MongoDB_USR = "user";
+	public String MongoDB_PWD = "usis";
 
 	public void setIP(String IP) {
 		MongoDB_IP = IP;
@@ -38,13 +36,28 @@ public class Connection {
 
 	public DB getDB() {
 		return MongoDB_DB;
-
+	}
+	public void setUSR(String USR) {
+		MongoDB_USR = USR;
 	}
 
+	public String getUSR() {
+		return MongoDB_USR;
+	}
+	public void setPWD(String PWD) {
+		MongoDB_PWD = PWD;
+	}
+
+	public String getPWD() {
+		return MongoDB_PWD;
+	}
+
+	@SuppressWarnings("deprecation")
 	public void DBConn() {
 		try {
 			Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
 			mongoLogger.setLevel(Level.SEVERE);
+			@SuppressWarnings("resource")
 			MongoClient mgClient = new MongoClient(new ServerAddress(getIP(), getPort()));
 			MongoDB_DB = mgClient.getDB("test");
 			System.out.println("mongoDB Connect");
